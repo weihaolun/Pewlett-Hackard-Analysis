5ff37f5dca7fefc7858004ab2d93a9fb102eea80
weihaolun <haolun@hlwei.me>
2021-08-03 19:42:18 -1000

    updated code

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -10,7 +10,8 @@ INTO retirement_titles
 FROM employees AS emp
 	INNER JOIN titles AS t
 		ON emp.emp_no = t.emp_no
-WHERE emp.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+WHERE (emp.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.to_date = '9999-01-01')
 ORDER BY emp_no ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -49,7 +50,10 @@ AND emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY emp_no ASC;
 	
 	
-	
+-- Deliverable 3
+-- Mentor eligible count
+SELECT COUNT (emp_no) AS "eligible count"
+FROM mentor_eligi;
 	
 	
 	
